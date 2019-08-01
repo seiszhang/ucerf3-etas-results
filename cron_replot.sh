1b655324e76405e6f321685bcbcb410b20499039
Kevin Milner <kmilner@usc.edu>
2019-08-01 15:33:35 -0700

    automatic plot and index update from opensha.usc.edu

--- cron_replot.sh
+++ cron_replot.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "******************************************"
+echo "*******  UCERF3-ETAS PLOT CRON JOB *******"
+echo "******************************************"
+date
+echo "******************************************"
+export ETAS_LAUNCHER=/home/scec-02/kmilner/ucerf3/ucerf3-etas-launcher
+export ETAS_SIM_DIR=/home/scec-02/kmilner/ucerf3/etas_sim
+
 LOCKFILE=/tmp/${USER}_etas_lock.txt
 if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
     echo "already running"
@@ -16,6 +24,10 @@ cd $DIR
 
 ./replot_build_index.sh $@
 
+echo
+echo "************* DONE PLOTTING **************"
+date
+
 if [[ $? -eq 0 ]];then
 	echo "pulling any updates from GitHub"
 	git pull
@@ -26,5 +38,9 @@ if [[ $? -eq 0 ]];then
 	echo "pushing to GitHub"
 	git push
 fi
+
+echo
+echo "******************* DONE *****************"
+date
 
 rm -f ${LOCKFILE}
